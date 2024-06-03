40a36f5702ba631321de5adea2ddec2cf9a21c5e
Palvi <psabherwal@ucsb.edu>
2024-06-03 13:33:19 -0700

    updating

--- Bowling Game/Assets/ballsfx.cs
+++ Bowling Game/Assets/ballsfx.cs
@@ -1,24 +1,20 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class ballsfx : MonoBehaviour
+public class BallSFX : MonoBehaviour
 {
-    public float gutterY = -0.03f;
-    public float tolerance = 0.01f; 
-    public AudioSource audioSource;
-    // Start is called before the first frame update
+    public float groundY = -0.03f; // Adjust this value to match your ground level
+    public AudioClip audioClip;
+    private AudioSource audioSource;
+
     void Start()
     {
-        //audioSource = gameObject.AddComponent<AudioSource>();
-        //audioSource.clip = audioClip;
-         
+        audioSource = gameObject.AddComponent<AudioSource>();
+        audioSource.clip = audioClip;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (Mathf.Abs(transform.position.y - gutterY) <= tolerance)
+        if (transform.position.y <= groundY)
         {
             if (!audioSource.isPlaying)
             {
